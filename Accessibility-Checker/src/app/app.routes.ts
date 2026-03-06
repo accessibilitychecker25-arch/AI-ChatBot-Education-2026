5ffc20922eb9c29fddaada137c534115b36fb149
accessibilitychecker25-arch <accessibilitychecker25@gmail.com>
2026-03-05 20:08:53 -0600

    Upload Cycle 2 Testing project contents to lily-branch

---
+++ Accessibility-Checker/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { AboutComponent } from './about/about.component';
+import { BatchUploadComponent } from './batch-upload/batch-upload.component';
+import { AuthGuard } from './auth.guard';
+
+export const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'batch-upload', component: BatchUploadComponent },
+      { path: 'about', component: AboutComponent },
+    ],
+  },
+];
